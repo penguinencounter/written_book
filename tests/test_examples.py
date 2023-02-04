394e8a4b17b172946721fe058b4ad1e5f7982fc0
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2023-02-04 10:04:26 -0800

    Empty example directory and skipping invalid examples

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -8,6 +8,8 @@ from pytest_insta import SnapshotFixture
 
 @pytest.mark.parametrize("directory", os.listdir("examples"))
 def test_build(snapshot: SnapshotFixture, directory: str):
+    if os.path.isfile(f"examples/{directory}"):
+        pytest.skip("Not a directory, so can't test examples on it.")
     with run_beet(directory=f"examples/{directory}") as ctx:
         document = ctx.inject(Document)
         document.markdown_serializer.flat = True
